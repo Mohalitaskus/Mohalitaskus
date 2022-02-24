f9d08dcc718494ba4ee0222d97f762870d9b9c7f
Mohalitaskus <100186963+Mohalitaskus@users.noreply.github.com>
2022-02-24 12:21:37 +0530

    Add files via upload
    Prohance and Workspace Installer

---
+++ abc.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+echo "Download Worksapce one intelligent hub"
+powershell -command "Invoke-WebRequest https://packages.vmware.com/wsone/AirwatchAgent.msi -OutFile AirwatchAgent.msi"
+echo "Done"
+cls
+pause
